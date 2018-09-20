52427f15be575bba533e1b8b3b9ad1eba389e25f
Lyuboslav Kotsev <liumbiwe@gmail.com>
2018-09-20 18:02:10 +0300

    Queue implemented.

--- src/Queue/Queue.php
+++ src/Queue/Queue.php
@@ -6,4 +6,74 @@ namespace DS\Queue;
 class Queue
 {
 
+    /**
+     * @var \SplFixedArray
+     */
+    private $queue;
+
+    /**
+     * @var int
+     */
+    private $capacity;
+
+    /**
+     * @var int
+     */
+    private $count = 0;
+
+    public function __construct($capacity = 1024)
+    {
+        $this->capacity = $capacity;
+
+        $this->queue = new \SplFixedArray($capacity);
+    }
+
+    /**
+     * Sets a waiting element.
+     *
+     * @param $data
+     */
+    public function enqueue($data)
+    {
+        $this->queue->offsetSet($this->count, $data);
+        $this->count++;
+    }
+
+    /**
+     * Returns the element at the front.
+     *
+     * @return mixed
+     */
+    public function dequeue()
+    {
+        $item = $this->queue->offsetGet(0);
+        $this->queue->offsetUnset(0);
+
+        for ($i = 1; $i <= $this->count; $i++) {
+            $v = $this->queue->offsetGet($i);
+            $this->queue->offsetSet($i - 1, $v);
+            $this->queue->offsetUnset($i);
+        }
+
+        $this->count--;
+
+        return $item;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCapacity()
+    {
+        return $this->capacity;
+    }
+
+    /**
+     * @return int
+     */
+    public function length()
+    {
+        return $this->count + 1;
+    }
+
 }
